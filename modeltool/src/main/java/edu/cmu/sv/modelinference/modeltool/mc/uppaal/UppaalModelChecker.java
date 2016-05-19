59ca5275007e4d6748f265eebeaf4022300fc9e8
Kasper Luckow <ksluckow@gmail.com>
2016-05-18 23:40:31 -0700

    Start clean up

--- modeltool/src/main/java/edu/cmu/sv/modelinference/modeltool/mc/uppaal/UppaalModelChecker.java
+++ modeltool/src/main/java/edu/cmu/sv/modelinference/modeltool/mc/uppaal/UppaalModelChecker.java
@@ -58,55 +58,55 @@ public class UppaalModelChecker extends ModelCheckerAdapter<NTA, String> {
 
 	private static final String TMP_FOLDR = "/tmp/";
 	private static final String UPPAAL_ENGINE_NAME = "verifyta";
-	
-	private static final int PROBABILITY_FACTOR = 100;
-	private static final String PC_LOC_PREFIX = "PC";
-	private static final String FINAL_LOC_PREFIX = "Final";
-	private static final String NOND_LOC_PREFIX = "Nondet";
-	
-  private static Logger logger = LoggerFactory.getLogger(UppaalModelChecker.class.getName());
-	
+
+//	private static final int PROBABILITY_FACTOR = 100;
+//	private static final String PC_LOC_PREFIX = "PC";
+//	private static final String FINAL_LOC_PREFIX = "Final";
+//	private static final String NOND_LOC_PREFIX = "Nondet";
+
+	private static Logger logger = LoggerFactory.getLogger(UppaalModelChecker.class.getName());
+
 	private NTA nta;
 	private Location initLoc;
-	private Update initUpdate;
+//	private Update initUpdate;
 	private Automaton automaton;
 	private Map<State, Location> translatedSources = new HashMap<>();
-	 private Map<State, Location> translatedDestinations = new HashMap<>(); //fix this mess
+	private Map<State, Location> translatedDestinations = new HashMap<>(); //fix this mess
 	private String targetMethod = "target";
-  private boolean isFirstLocSet = false;
-	
+	private boolean isFirstLocSet = false;
+
 	@Override
 	public void initModelGenerator(Model<?> model) {
 		this.nta = new NTA();
 		this.automaton = new Automaton(targetMethod);
 		this.nta.setSystemName(targetMethod + "_model");
-		
-    final int defaultInitVal = 0;
-    int initVal;
+
+		final int defaultInitVal = 0;
+		int initVal;
 		LinkedList<String> updates = new LinkedList<>();
 		for(String modelVar : model.getModelVariables()) {
-		  //TODO: Assume integer variables ONLY
-      int lowerBound = model.getMinAssignedVal(modelVar);
-      int upperBound = model.getMaxAssignedVal(modelVar);
+			//TODO: Assume integer variables ONLY
+			int lowerBound = model.getMinAssignedVal(modelVar);
+			int upperBound = model.getMaxAssignedVal(modelVar);
 
-      if(upperBound == 0) {
-        upperBound++;
-        logger.warn("Upper bound for variable [" + modelVar + "] is 0 and is therefore superfluous! Its value is incremented now to avoid PRISM errors.");
-      } else if(lowerBound == upperBound) {
-        //logger.warn("Upper and lower bound for variable [" + modelVar + "] is the same! The variable is superfluous.");
-      }
-      
-      //TODO: get init val. Super ugly
-      Assignment<?> var = model.getInitState().getAssignment(modelVar);
-      if(var != null)
-        initVal = Integer.valueOf(var.getValue().toString());
-      else
-        initVal = defaultInitVal;
-      
-      this.automaton.getDeclaration().add("int[" + lowerBound + "," + upperBound + "] " + modelVar + " = " + initVal + ";");
-      updates.add(modelVar);
+			if(upperBound == 0) {
+				upperBound++;
+				logger.warn("Upper bound for variable [" + modelVar + "] is 0 and is therefore superfluous! Its value is incremented now to avoid PRISM errors.");
+			} else if(lowerBound == upperBound) {
+				//logger.warn("Upper and lower bound for variable [" + modelVar + "] is the same! The variable is superfluous.");
+			}
+
+			//TODO: get init val. Super ugly
+			Assignment<?> var = model.getInitState().getAssignment(modelVar);
+			if(var != null)
+				initVal = Integer.valueOf(var.getValue().toString());
+			else
+				initVal = defaultInitVal;
+
+			this.automaton.getDeclaration().add("int[" + lowerBound + "," + upperBound + "] " + modelVar + " = " + initVal + ";");
+			updates.add(modelVar);
 		}
-		
+
 		//this.initUpdate = new Update(updateStr);
 		this.initLoc = new Location(this.automaton, "initloc");
 		this.translatedSources.put(model.getInitState(), this.initLoc);
@@ -138,86 +138,86 @@ public class UppaalModelChecker extends ModelCheckerAdapter<NTA, String> {
 
 		return stdoutString + stderrString;
 	}
-	
+
 	@Override
 	public void finishModelGenerator(Model<?> irModel) {
-	  for(State s : irModel.getStates()) {
-	    Location srcLoc = this.translatedSources.get(s);
-	    for(edu.cmu.sv.modelinference.common.model.Transition t : s.getOutgoingTransitions()) {
-	      if(!(t instanceof WeightedTransition)) {
-	        throw new UppaalModelGeneratorException("Expected weighted transition");
-	      }
-        WeightedTransition wt = (WeightedTransition)t;
-	      State nextState = t.getDest();
-	      Location destLoc = this.translatedDestinations.get(nextState);
+		for(State s : irModel.getStates()) {
+			Location srcLoc = this.translatedSources.get(s);
+			for(edu.cmu.sv.modelinference.common.model.Transition t : s.getOutgoingTransitions()) {
+				if(!(t instanceof WeightedTransition)) {
+					throw new UppaalModelGeneratorException("Expected weighted transition");
+				}
+				WeightedTransition wt = (WeightedTransition)t;
+				State nextState = t.getDest();
+				Location destLoc = this.translatedDestinations.get(nextState);
 
-	      Transition trans = new Transition(this.automaton, srcLoc, destLoc);
-	      trans.setProb(new Probability(wt.getWeight()));
-	    }
-	  }
+				Transition trans = new Transition(this.automaton, srcLoc, destLoc);
+				trans.setProb(new Probability(wt.getWeight()));
+			}
+		}
 	}
 
-  @Override
-  public <S extends State> void visit(S state) {
-    Location branchLoc = new Location(this.automaton);
-    branchLoc.setBranchPointLocation(true);
-    this.translatedSources.put(state, branchLoc);
-    Location translatedLoc = new Location(this.automaton, state.toString().replaceAll("[\\s,=]*", ""));
-    translatedLoc.setType(LocationType.URGENT);
-    new Transition(this.automaton, translatedLoc, branchLoc);
-    if(!isFirstLocSet) {
-      new Transition(this.automaton, this.initLoc, translatedLoc);
-      isFirstLocSet = true;
-    }
-    this.translatedDestinations.put(state, translatedLoc);
-  }
-  
-  @Override
-  public void visit(edu.cmu.sv.modelinference.common.model.Transition transition) {
-    // TODO Auto-generated method stub
-    
-  }
-  @Override
-  public <S extends State> void visit(Model<S> model) {
-    // TODO Auto-generated method stub
-  }
-  
-  @Override
-  protected ModelAdapter<NTA> getGeneratedModel() {
-    return new UppaalModel(this.nta);
-  }
-  
-  @Override
-  public ModelCheckerResult executeModelChecker(ModelAdapter<NTA> model,
-      Collection<PropertyAdapter<String>> properties) throws UppaalModelCheckerException {
-    String OS = System.getProperty("os.name").toLowerCase();
-    boolean isUnix = OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0;
-    boolean isMac = OS.indexOf("mac") >= 0;
-    //TODO: Fix this restriction...
-    if(!isUnix && !isMac)
-      throw new UppaalModelCheckerException("Currently, uppaal can only be run on Mac or Linux/Unix");
-    if(!Files.exists(Paths.get(TMP_FOLDR)))
-      throw new UppaalModelCheckerException("To run the verification engine of uppaal directly, you must have write access to " + TMP_FOLDR);
-    
-    String modelPath = "";
-    String queryPath = "";
-    //TODO: we should reuse the model files written to the output path if that is specified in the config. This would 
-    //prevent writing the files twice.
-    try {
-      model.writeModelToFile(TMP_FOLDR);
-    } catch (IOException e1) {
-      throw new UppaalModelCheckerException(e1);
-    }
+	@Override
+	public <S extends State> void visit(S state) {
+		Location branchLoc = new Location(this.automaton);
+		branchLoc.setBranchPointLocation(true);
+		this.translatedSources.put(state, branchLoc);
+		Location translatedLoc = new Location(this.automaton, state.toString().replaceAll("[\\s,=]*", ""));
+		translatedLoc.setType(LocationType.URGENT);
+		new Transition(this.automaton, translatedLoc, branchLoc);
+		if(!isFirstLocSet) {
+			new Transition(this.automaton, this.initLoc, translatedLoc);
+			isFirstLocSet = true;
+		}
+		this.translatedDestinations.put(state, translatedLoc);
+	}
 
-    Process process;
-    try {
-      process = Runtime.getRuntime().exec(UPPAAL_ENGINE_NAME + " " + modelPath + " " + queryPath);
-      String output = getOutput(process);
-    } catch (IOException e) {
-      throw new UppaalModelCheckerException(e);
-    }    
+	@Override
+	public void visit(edu.cmu.sv.modelinference.common.model.Transition transition) {
+		// TODO Auto-generated method stub
 
-    //FIX this
-    return null;
-  }
+	}
+	@Override
+	public <S extends State> void visit(Model<S> model) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	protected ModelAdapter<NTA> getGeneratedModel() {
+		return new UppaalModel(this.nta);
+	}
+
+	@Override
+	public ModelCheckerResult executeModelChecker(ModelAdapter<NTA> model,
+			Collection<PropertyAdapter<String>> properties) throws UppaalModelCheckerException {
+		String OS = System.getProperty("os.name").toLowerCase();
+		boolean isUnix = OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0;
+		boolean isMac = OS.indexOf("mac") >= 0;
+		//TODO: Fix this restriction...
+		if(!isUnix && !isMac)
+			throw new UppaalModelCheckerException("Currently, uppaal can only be run on Mac or Linux/Unix");
+		if(!Files.exists(Paths.get(TMP_FOLDR)))
+			throw new UppaalModelCheckerException("To run the verification engine of uppaal directly, you must have write access to " + TMP_FOLDR);
+
+		String modelPath = "";
+		String queryPath = "";
+		//TODO: we should reuse the model files written to the output path if that is specified in the config. This would 
+		//prevent writing the files twice.
+		try {
+			model.writeModelToFile(TMP_FOLDR);
+		} catch (IOException e1) {
+			throw new UppaalModelCheckerException(e1);
+		}
+
+		Process process;
+		try {
+			process = Runtime.getRuntime().exec(UPPAAL_ENGINE_NAME + " " + modelPath + " " + queryPath);
+			String output = getOutput(process);
+		} catch (IOException e) {
+			throw new UppaalModelCheckerException(e);
+		}    
+
+		//FIX this
+		return null;
+	}
 }
